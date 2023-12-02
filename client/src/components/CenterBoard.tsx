f64147fecbd0996bb50d700799578bd29ce6af3c
jingxianlau <jingxian.lau16@gmail.com>
2023-12-02 13:03:59 +0800

    center board updated styling and translucent background

--- client/src/components/CenterBoard.tsx
+++ client/src/components/CenterBoard.tsx
@@ -48,85 +48,88 @@ const CenterBoard: React.FC<CenterBoardProps> = ({
 
   return (
     <div className='mat'>
-      {state.val.mainDeck.monsters.map(card => (
-        <div
-          className='large'
-          key={card.id}
-          onMouseEnter={() => {
-            shownCard.set(card);
-            shownCard.setPos('left');
-          }}
-          onMouseLeave={() => {
-            shownCard.set(null);
-            shownCard.setPos(null);
-          }}
-        >
-          <img
-            src={getImage(card)}
-            alt={card.name}
-            className='large-card'
-            draggable='false'
-          />
-        </div>
-      ))}
-      {Array.from(Array(3 - state.val.mainDeck.monsters.length), (_, i) => (
-        <div className='large' key={i}></div>
-      ))}
-
-      <div className='small-cards'>
-        <div className='small deck'>
-          <img
-            src='./assets/back/back-creme.png'
-            alt='flipped card'
-            className={`small-card ${
-              (state.val.turn.phase === 'draw' &&
-                state.val.turn.player === state.val.playerNum) ||
-              (state.val.turn.phase === 'use-effect' &&
-                state.val.turn.effect &&
-                state.val.turn.effect.action === 'draw' &&
-                state.val.turn.effect.players.some(
-                  val => val === state.val.playerNum
-                ))
-                ? 'glow click'
-                : state.val.turn.player === state.val.playerNum &&
-                  state.val.turn.phase === 'play'
-                ? 'click'
-                : ''
-            }`}
-            onClick={
-              state.val.turn.phase === 'draw'
-                ? drawTwo
-                : state.val.turn.phase === 'play'
-                ? drawOne
-                : state.val.turn.phase === 'use-effect' &&
-                  state.val.turn.effect &&
-                  state.val.turn.effect.action === 'draw'
-                ? drawEffect
-                : () => {}
-            }
-            draggable='false'
-          />
-        </div>
-        <div className='small discard'>
-          {state.val.mainDeck.discardPile.length > 0 && (
+      <div className='background'></div>
+      <div className='cards'>
+        {state.val.mainDeck.monsters.map(card => (
+          <div
+            className='large'
+            key={card.id}
+            onMouseEnter={() => {
+              shownCard.set(card);
+              shownCard.setPos('left');
+            }}
+            onMouseLeave={() => {
+              shownCard.set(null);
+              shownCard.setPos(null);
+            }}
+          >
             <img
-              src={getImage(
-                state.val.mainDeck.discardPile[
-                  state.val.mainDeck.discardPile.length - 1
-                ]
-              )}
-              alt={
-                state.val.mainDeck.discardPile[
-                  state.val.mainDeck.discardPile.length - 1
-                ].name
-              }
-              className='small-card click'
-              onClick={() => {
-                setShowDiscardPile(true);
-              }}
+              src={getImage(card)}
+              alt={card.name}
+              className='large-card'
               draggable='false'
             />
-          )}
+          </div>
+        ))}
+        {Array.from(Array(3 - state.val.mainDeck.monsters.length), (_, i) => (
+          <div className='large' key={i}></div>
+        ))}
+
+        <div className='small-cards'>
+          <div className='small deck'>
+            <img
+              src='./assets/back/back-creme.png'
+              alt='flipped card'
+              className={`small-card ${
+                (state.val.turn.phase === 'draw' &&
+                  state.val.turn.player === state.val.playerNum) ||
+                (state.val.turn.phase === 'use-effect' &&
+                  state.val.turn.effect &&
+                  state.val.turn.effect.action === 'draw' &&
+                  state.val.turn.effect.players.some(
+                    val => val === state.val.playerNum
+                  ))
+                  ? 'glow click'
+                  : state.val.turn.player === state.val.playerNum &&
+                    state.val.turn.phase === 'play'
+                  ? 'click'
+                  : ''
+              }`}
+              onClick={
+                state.val.turn.phase === 'draw'
+                  ? drawTwo
+                  : state.val.turn.phase === 'play'
+                  ? drawOne
+                  : state.val.turn.phase === 'use-effect' &&
+                    state.val.turn.effect &&
+                    state.val.turn.effect.action === 'draw'
+                  ? drawEffect
+                  : () => {}
+              }
+              draggable='false'
+            />
+          </div>
+          <div className='small discard'>
+            {state.val.mainDeck.discardPile.length > 0 && (
+              <img
+                src={getImage(
+                  state.val.mainDeck.discardPile[
+                    state.val.mainDeck.discardPile.length - 1
+                  ]
+                )}
+                alt={
+                  state.val.mainDeck.discardPile[
+                    state.val.mainDeck.discardPile.length - 1
+                  ].name
+                }
+                className='small-card click'
+                onClick={() => {
+                  setShowDiscardPile(true);
+                }}
+                draggable='false'
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
